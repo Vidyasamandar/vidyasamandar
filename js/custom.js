1c671aa992284d50b17f7bc380b90d1be3b80a23
vidyasamandar <vidyasamandar@gmail.com>
2023-10-18 12:11:12 +0530

    [UPDT] styles

--- js/custom.js
+++ js/custom.js
@@ -47,7 +47,26 @@
       var timeline = $('#vertical-scrollable-timeline li');
       Array.from(timeline).forEach(isScrollIntoView);
     });
-  
+    const cursorDot = document.getElementById('cursor-dot');
+    var mouseX = 0
+    var mouseY = 0
+    var pY = 0
+    var pX = 0
+
+    // Function to update the cursor-dot position
+    function moveCursorDot(event) {
+        mouseX = (event.clientX - cursorDot.clientWidth / 1.5);
+        mouseY = (event.clientY - cursorDot.clientHeight / 1.5);
+    }
+    setInterval(function(){
+      pY += (mouseY - pY) / 15
+      pX += (mouseX - pX) / 15
+      cursorDot.style.left = pX + 'px'
+      cursorDot.style.top = pY + 'px'
+    }, 10)
+    
+    // Add an event listener to track cursor movement
+    document.addEventListener('mousemove', moveCursorDot);
   })(window.jQuery);
 
 
